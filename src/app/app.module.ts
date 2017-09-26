807229c03f985ee127c38ae30eed9f276d5891ad
jeans <jean.stam@libertymutual.com>
2017-09-26 11:50:53 -0700

    Building Angular ApartmentList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule} from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ApartmentListingsComponent } from './apartment-listings/apartment-listings.component';
+
+import { ApartmentDataService } from './apartment-data/apartment-data.service';
+import { ApartmentDetailComponent } from './apartment-detail/apartment-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ApartmentListingsComponent,
+    ApartmentDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ ApartmentDataService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
